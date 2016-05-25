1590457b5aaf865f70ee790159a7850d898b3662
Christian Böttcher <c.boettcher@fz-juelich.de>
2016-05-25 15:27:24 +0200

    XML Streams vom Server kopiert

---
+++ de/teamkaesekaestchen/rnvl/io/XmlOutStream.java
@@ -0,0 +1,55 @@
+package de.teamkaesekaestchen.rnvl.io;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+
+import de.teamkaesekaestchen.rnvl.prot.MazeCom;
+
+public class XmlOutStream extends UTFOutputStream {
+
+	private Marshaller marshaller;
+
+	public XmlOutStream(OutputStream out) {
+		super(out);
+		// Anlegen der JAXB-Komponenten
+		try {
+			JAXBContext jaxbContext = JAXBContext.newInstance(MazeCom.class);
+			this.marshaller = jaxbContext.createMarshaller();
+			this.marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT,
+					Boolean.TRUE);
+		} catch (JAXBException e) {
+//			Debug.print(
+//					Messages.getString("XmlOutStream.ErrorInitialisingJAXBComponent"), DebugLevel.DEFAULT); //$NON-NLS-1$
+		}
+	}
+
+	/**
+	 * Versenden einer XML Nachricht
+	 * 
+	 * @param mc
+	 */
+	public void write(MazeCom mc) {
+		// generierung des fertigen XML
+		try {
+			ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+			this.marshaller.marshal(mc, byteArrayOutputStream);
+//			Debug.print(
+//					Messages.getString("XmlOutStream.Written"), DebugLevel.DEBUG); //$NON-NLS-1$
+//			Debug.print(new String(byteArrayOutputStream.toByteArray()), DebugLevel.DEBUG);
+			// Versenden des XML
+			this.writeUTF8(new String(byteArrayOutputStream.toByteArray()));
+			this.flush();
+		} catch (IOException e) {
+//			Debug.print(
+//					Messages.getString("XmlOutStream.errorSendingMessage"), DebugLevel.DEFAULT); //$NON-NLS-1$
+		} catch (JAXBException e1) {
+			e1.printStackTrace();
+		}
+	}
+
+}
