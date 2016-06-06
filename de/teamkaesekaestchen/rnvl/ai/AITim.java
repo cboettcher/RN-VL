e3ced5fad28fa79a44b283d78eb5aae8ad69b5d0
Tim Erdweg <mcquadrat@gmx.de>
2016-06-06 11:31:48 +0200

    Random AITim verbessert

--- de/teamkaesekaestchen/rnvl/ai/AITim.java
+++ de/teamkaesekaestchen/rnvl/ai/AITim.java
@@ -1,8 +1,10 @@
 package de.teamkaesekaestchen.rnvl.ai;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
+import de.teamkaesekaestchen.rnvl.impl.*;
 import de.teamkaesekaestchen.rnvl.net.Main;
 import de.teamkaesekaestchen.rnvl.prot.*;
 
@@ -10,63 +12,111 @@ public class AITim implements Player {
 	
 	private static final Logger logger  = Logger.getLogger("AITim");
 	
-	private int[] aktpos = new int[2];
-	private int[] treasurepos = new int[2];
+	private Position aktpos;
+	private Position treasurepos;
+	
+	private Board bt;
 
 	@Override
-	public MoveMessageType getZug(BoardType bt, TreasureType tt,
+	public MoveMessageType getZug(BoardType btt, TreasureType tt,
 			List<TreasureType> foundTT, List<TreasuresToGoType> togoTT) {
 		
 		logger.info("Ai Tim ist gestartet");
-		System.out.println(Main.getFailedMoves());
-		if(Main.getFailedMoves()>5){
-			System.exit(1);
-		}
 		
-		for (int i = 0; i < 7; i++){
-			for (int j = 0; j < 7; j++){
-				if(bt.getRow().get(i).getCol().get(j).getTreasure() != null){
-					if (bt.getRow().get(i).getCol().get(j).getTreasure().equals(tt)){
-						treasurepos[0] = i;
-						treasurepos[1] = j;
-						
-					}
-				}
-				if(bt.getRow().get(i).getCol().get(j).getPin().getPlayerID().size() != 0){
-					for(Integer id :bt.getRow().get(i).getCol().get(j).getPin().getPlayerID()){
-						if(id == Main.id){
-							aktpos[0] = i;
-							aktpos[1] = j;
-						}
-					}
-				}
-				
-			}
-		}
-		if(bt.getForbidden() != null){
-			System.out.println("Forbidden: "+bt.getForbidden().getCol() + "  -  "+bt.getForbidden().getRow());
-		}
+		this.bt = new Board(btt);
 		
-		System.out.println(aktpos[0]+"  -  "+aktpos[1]+"\n"+treasurepos[0]+"  -  -  "+treasurepos[1]);
+//		System.out.println(Main.getFailedMoves());
+//		if(Main.getFailedMoves()>5){
+//			System.exit(1);
+//		}
+		
+		aktpos = new Position(bt.findPlayer(Main.id));
+		treasurepos = new Position(bt.findTreasure(tt));
+		
+//		for(PositionType poo : bt.getAllReachablePositions(p)){
+//			System.out.println("Moeglich: ("+poo.getRow()+"|"+poo.getCol()+")");
+//		}
 		
 		
 		MoveMessageType mmt = new MoveMessageType();
+		List<MoveMessageType> moveList = new ArrayList<>();
 		
-		PositionType pt = new PositionType();
-		pt.setCol(aktpos[1]);
-		pt.setRow(aktpos[0]);
-		mmt.setNewPinPos(pt);
+		mmt.setNewPinPos(aktpos);
+		boolean gefunden = false;
+		for(int i : new int[]{0, 6}){
+			for(int j : new int[]{1, 3, 5}){
+				if(bt.getForbidden()!= null &&bt.getForbidden().getCol() == j && bt.getForbidden().getRow() == i){
+					continue;
+				}
+				Card shiftCard = new Card(bt.getShiftCard());
+				for(Card c : shiftCard.getPossibleRotations()){
+					mmt.setShiftCard(c);
+					mmt.setShiftPosition(new Position(i, j));
+					Board newBoard = bt.fakeShift(mmt);//Spalten
+					aktpos = new Position(bt.findPlayer(Main.id));
+					treasurepos = new Position(bt.findTreasure(tt));
+					for(PositionType pp : newBoard.getAllReachablePositions(aktpos)){
+						if(pp.getCol() == treasurepos.getCol() && pp.getRow() == treasurepos.getRow()){
+							mmt.setNewPinPos(new Position(treasurepos));
+							return mmt;
+						}
+					}
+					moveList.add(new MoveMessage(mmt));
+				}
+				
+				if(bt.getForbidden()!= null &&bt.getForbidden().getCol() == i && bt.getForbidden().getRow() == j){
+					continue;
+				}
+				shiftCard = new Card(bt.getShiftCard());
+				for(Card c : shiftCard.getPossibleRotations()){
+					mmt.setShiftPosition(new Position(j, i));//Reihen
+					Board newBoard = bt.fakeShift(mmt);
+					aktpos = new Position(bt.findPlayer(Main.id));
+					treasurepos = new Position(bt.findTreasure(tt));
+					for(PositionType pp : newBoard.getAllReachablePositions(aktpos)){
+						if(pp.getCol() == treasurepos.getCol() && pp.getRow() == treasurepos.getRow()){
+							mmt.setNewPinPos(new Position(treasurepos));
+							return mmt;
+						}
+					}
+					moveList.add(new MoveMessage(mmt));
+					
+				}
+			}
+		}
 		
-		pt = new PositionType();
-		pt.setCol(0);
-		pt.setRow(1);
-		mmt.setShiftPosition(pt);
+		mmt = moveList.get((int)(Math.random()*moveList.size()));
 		
-		mmt.setShiftCard(bt.getShiftCard());
+		mmt.setNewPinPos(aktpos);
+		Board newBoard = bt.fakeShift(mmt);
 		
+		List<PositionType> positionList = newBoard.getAllReachablePositions(aktpos);
+		mmt.setNewPinPos(positionList.get((int)(Math.random()*positionList.size())));
+		
+//		mmt.setNewPinPos(pt);
+//		
+//		pt = new PositionType();
+//		pt.setCol(0);
+//		pt.setRow(1);
+//
+//		if(bt.getForbidden() != null){
+//			System.out.println("Forbidden: "+bt.getForbidden().getCol() + "  -  "+bt.getForbidden().getRow());
+//			if(bt.getForbidden().getCol()==6 && bt.getForbidden().getRow()==1){
+//				pt.setRow(3);
+//			}
+//		}
+//		mmt.setShiftPosition(pt);
+//		
+//		mmt.setShiftCard(bt.getShiftCard());
+		System.out.println("aktpos:            "+aktpos.getRow()+"  "+aktpos.getCol());
+		System.out.println("pinpos:            "+mmt.getNewPinPos().getRow()+" "+mmt.getNewPinPos().getCol());
+		System.out.println("shiftCardPosition: "+mmt.getShiftPosition().getRow()+"  "+mmt.getShiftPosition().getCol());
 		
 		
 		return mmt;
 	}
-
+	
+	
+	
+	
 }
